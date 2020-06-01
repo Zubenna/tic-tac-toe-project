8596e20c501ad8f1254a2c4202a7ec39e23ac2c7
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-01 17:10:34 -0500

    Game board start

--- bin/main.rb
+++ bin/main.rb
@@ -4,14 +4,39 @@
 # Set the game board
 # Load the Players
 
-puts 'Player one select "x" or "o"'
-first_player = gets.chomp
-second_player = ""
-if first_player == 'X'
-   second_player = 'O'
-puts "Player one selected #{first_player} and player two selected #{second_player}"
-elsif first_player == 'O'
-second_player = 'X'
-puts "Player one selected #{first_player} and player two selected #{second_player}"
+puts "Welcome to TIC-TAC-TOE GAME"
+loop do
+  puts 'Player one select "x" or "o"'
+  first_player = gets.chomp
+  second_player = ""
+  if first_player == 'X'
+    second_player = 'O'
+    puts "Player one selected #{first_player} and player two selected #{second_player}"
+  elsif first_player == 'O'
+    second_player = 'X'
+    puts "Player one selected #{first_player} and player two selected #{second_player}"
+  else
+    puts "Please select X or O"
+  end
+  break if first_player == "O" || first_player == "X"
 end
+
+puts "1     |    |    "
+puts a = "      |    |    "
+puts "  --------------"
+puts "2     |    |    "
+puts b = "      |    |    "
+puts "  --------------"
+puts "      |    |    "
+puts c = "3     |    |    "
+puts "    a    b    c "
+
+puts "Please select the coordinates where you want to play"
+puts "Example: 1b or 2c"
+player_gets = gets.chomp
+
+if player_gets == "1a"
+  a = "    X  |    |    " 
+end
+
 
