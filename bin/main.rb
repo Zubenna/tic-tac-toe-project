649ee127ec8ba4b3e019410b02376e0684008ccf
Nnamdi <emelunna@gmail.com>
2020-06-01 22:39:45 +0100

    added prompt for player selection

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+# Load the game
+# Set the game board
+# Load the Players
+
+puts 'Player one select "x" or "o"'
+first_player = gets.chomp
+second_player = ""
+if first_player == 'X'
+   second_player = 'O'
+puts "Player one selected #{first_player} and player two selected #{second_player}"
+elsif first_player == 'O'
+second_player = 'X'
+puts "Player one selected #{first_player} and player two selected #{second_player}"
+end
+
