ecceff6e2faf3864e001e6c2b1c6d4a245e8dbf1
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-02 10:47:43 -0500

    Board finish

--- bin/main.rb
+++ bin/main.rb
@@ -5,30 +5,48 @@
 puts "Welcome to TIC-TAC-TOE GAME"
 puts
 loop do
-  puts 'Player one select "x" or "o"'
-  first_player = gets.chomp
-  second_player = ""
-  if first_player == 'X'
-    second_player = 'O'
-    puts "Player one selected #{first_player} and player two selected #{second_player}"
-  elsif first_player == 'O'
-    second_player = 'X'
-    puts "Player one selected #{first_player} and player two selected #{second_player}"
+  puts 'Player one select "X" or "O"'
+  $first_player = gets.chomp
+  $second_player = ""
+  if $first_player == 'X'
+    $second_player = 'O'
+    puts "Player one selected #{$first_player} and player two selected #{$second_player}"
+  elsif $first_player == 'O'
+    $second_player = 'X'
+    puts "Player one selected #{$first_player} and player two selected #{$second_player}"
   else
     puts "Please select X or O"
   end
-  break if first_player == "O" || first_player == "X"
+  break if $first_player == "O" || $first_player == "X"
+end
+loop do 
+board = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+puts "  #{board[0][0]}  |  #{board[0][1]}  |  #{board[0][2]}"
+puts "================="
+puts "  #{board[1][0]}  |  #{board[1][1]}  |  #{board[1][2]}"
+puts "================="
+puts "  #{board[2][0]}  |  #{board[2][1]}  |  #{board[2][2]}"
+
+
+puts "Player one: please select the number where you want to play #{$first_player}"
+player_choose = gets.chomp
+player_choose = player_choose.to_i
+
+if player_choose <= 3 && player_choose >= 1
+  board[0][player_choose - 1] = $first_player
+  board = board
+elsif player_choose <= 6 && player_choose > 3
+  board[1][player_choose - 1] = $first_player
+  board = board
+elsif player_choose <= 9 && player_choose > 6
+  board[1][player_choose - 1] = $first_player
+  borad = board
 end
 
-board_array = [ ["", "", " "], [" ", " ", " "], [" ", " ", " "]]
-
-puts board_array
-
-puts "Please select the coordinates where you want to play"
-puts "Example: 1b or 2c"
-player_gets = gets.chomp
-
-if player_gets == "1a"
-  a = "    X  |    |    " 
+break if board.all?(String)
 end
+
+
+
 
