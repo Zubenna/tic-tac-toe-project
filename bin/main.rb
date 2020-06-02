224e412c74e5a802575c1275daccf35a691795c2
Nnamdi <emelunna@gmail.com>
2020-06-02 16:00:58 +0100

    defined board array

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
-# Load the game
-# Set the game board
-# Load the Players
+# The following classes will be implemented Main, player, and Board
 
 puts "Welcome to TIC-TAC-TOE GAME"
+puts
 loop do
   puts 'Player one select "x" or "o"'
   first_player = gets.chomp
@@ -21,15 +20,9 @@ loop do
   break if first_player == "O" || first_player == "X"
 end
 
-puts "1     |    |    "
-puts a = "      |    |    "
-puts "  --------------"
-puts "2     |    |    "
-puts b = "      |    |    "
-puts "  --------------"
-puts "      |    |    "
-puts c = "3     |    |    "
-puts "    a    b    c "
+board_array = [ ["", "", " "], [" ", " ", " "], [" ", " ", " "]]
+
+puts board_array
 
 puts "Please select the coordinates where you want to play"
 puts "Example: 1b or 2c"
@@ -38,5 +31,4 @@ player_gets = gets.chomp
 if player_gets == "1a"
   a = "    X  |    |    " 
 end
-
 
