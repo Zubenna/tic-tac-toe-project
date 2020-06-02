cdbc33d7a7363d1f15a7dba0ee3fe4b8a0b0cc54
Nnamdi <emelunna@gmail.com>
2020-06-02 17:20:57 +0100

    added loop termination condition

--- bin/main.rb
+++ bin/main.rb
@@ -2,51 +2,49 @@
 
 # The following classes will be implemented Main, player, and Board
 
-puts "Welcome to TIC-TAC-TOE GAME"
+puts 'Welcome to TIC-TAC-TOE GAME'
 puts
+first_player = ''
+second_player = ''
 loop do
   puts 'Player one select "X" or "O"'
-  $first_player = gets.chomp
-  $second_player = ""
-  if $first_player == 'X'
-    $second_player = 'O'
-    puts "Player one selected #{$first_player} and player two selected #{$second_player}"
-  elsif $first_player == 'O'
-    $second_player = 'X'
-    puts "Player one selected #{$first_player} and player two selected #{$second_player}"
+  first_player = gets.chomp
+  if first_player == 'X'
+    second_player = 'O'
+    puts "Player one selected #{first_player} and player two selected #{second_player}"
+  elsif first_player == 'O'
+    second_player = 'X'
+    puts "Player one selected #{first_player} and player two selected #{second_player}"
   else
-    puts "Please select X or O"
+    puts 'Please select X or O'
   end
-  break if $first_player == "O" || $first_player == "X"
+  break if first_player == 'O' || first_player == 'X'
 end
 loop do 
 board = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 puts "  #{board[0][0]}  |  #{board[0][1]}  |  #{board[0][2]}"
-puts "================="
+puts '================='
 puts "  #{board[1][0]}  |  #{board[1][1]}  |  #{board[1][2]}"
-puts "================="
+puts '================='
 puts "  #{board[2][0]}  |  #{board[2][1]}  |  #{board[2][2]}"
 
 
-puts "Player one: please select the number where you want to play #{$first_player}"
+puts 'Player one: please select the number where you want to play from the board'
+puts 'Enter 0 to quit'
 player_choose = gets.chomp
 player_choose = player_choose.to_i
 
 if player_choose <= 3 && player_choose >= 1
-  board[0][player_choose - 1] = $first_player
+  board[0][player_choose - 1] = first_player
   board = board
 elsif player_choose <= 6 && player_choose > 3
-  board[1][player_choose - 1] = $first_player
+  board[1][player_choose - 1] = first_player
   board = board
 elsif player_choose <= 9 && player_choose > 6
-  board[1][player_choose - 1] = $first_player
+  board[2][player_choose - 1] = first_player
   borad = board
 end
 
-break if board.all?(String)
+break if board.all?(String) || player_choose == 0
 end
-
-
-
-
