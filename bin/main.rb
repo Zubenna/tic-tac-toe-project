22da96d64b4e91dd3cd1490b00797ea1fc91ff43
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-03 18:31:16 -0500

    FIX-Stickler errors

--- bin/main.rb
+++ bin/main.rb
@@ -32,26 +32,26 @@ loop do
   puts '================='
   puts "  #{board[6]}  |  #{board[7]}  |  #{board[8]}"
   if (board[0].eql?(board[1]) && board[1].eql?(board[2])) || (board[3].eql?(board[4]) && board[4].eql?(board[5])) || (board[6].eql?(board[7]) && board[7].eql?(board[8]))
-    if board[0].eql?(first_player) || board[3].eql?(first_player) || board[6].eql?(first_player)
+    if (board[0].eql?(first_player) && board[1].eql?(first_player) && board[2].eql?(first_player)) || (board[3].eql?(first_player) && board[4].eql?(first_player) && board[5].eql?(first_player)) || (board[6].eql?(first_player) && board[7].eql?(first_player) && board[8].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[3].eql?(second_player) || board[6].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[1].eql?(second_player) && board[2].eql?(second_player)) || (board[3].eql?(second_player) && board[4].eql?(second_player) && board[5].eql?(second_player)) || (board[6].eql?(second_player) && board[7].eql?(second_player) && board[8].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
   elsif (board[0].eql?(board[3]) && board[3].eql?(board[6])) || (board[1].eql?(board[4]) && board[4].eql?(board[7])) || (board[2].eql?(board[5]) && board[5].eql?(board[8]))
-    if board[0].eql?(first_player) || board[1].eql?(first_player) || board[2].eql?(first_player)
+    if (board[0].eql?(first_player) && board[3].eql?(first_player) && board[6].eql?(first_player)) || (board[1].eql?(first_player) && board[4].eql?(first_player) && board[7].eql?(first_player)) || (board[2].eql?(first_player) && board[5].eql?(first_player) && board[8].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[1].eql?(second_player) || board[2].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[3].eql?(second_player) && board[6].eql?(second_player)) || (board[1].eql?(second_player) && board[4].eql?(second_player) && board[7].eql?(second_player)) || (board[2].eql?(second_player) && board[5].eql?(second_player) && board[8].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
   elsif board[0].eql?(board[4]) && board[4].eql?(board[8]) || board[2].eql?(board[4]) && board[4].eql?(board[6])
-    if board[0].eql?(first_player) || board[2].eql?(first_player)
+    if (board[0].eql?(first_player) && board[4].eql?(first_player) && board[8].eql?(first_player)) || (board[2].eql?(first_player) && board[4].eql?(first_player) && board[6].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[2].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[4].eql?(second_player) && board[8].eql?(second_player)) || (board[2].eql?(second_player) && board[4].eql?(second_player) && board[6].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
@@ -80,26 +80,26 @@ loop do
   puts '================='
   puts "  #{board[6]}  |  #{board[7]}  |  #{board[8]}"
   if (board[0].eql?(board[1]) && board[1].eql?(board[2])) || (board[3].eql?(board[4]) && board[4].eql?(board[5])) || (board[6].eql?(board[7]) && board[7].eql?(board[8]))
-    if board[0].eql?(first_player) || board[3].eql?(first_player) || board[6].eql?(first_player)
+    if (board[0].eql?(first_player) && board[1].eql?(first_player) && board[2].eql?(first_player)) || (board[3].eql?(first_player) && board[4].eql?(first_player) && board[5].eql?(first_player)) || (board[6].eql?(first_player) && board[7].eql?(first_player) && board[8].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[3].eql?(second_player) || board[6].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[1].eql?(second_player) && board[2].eql?(second_player)) || (board[3].eql?(second_player) && board[4].eql?(second_player) && board[5].eql?(second_player)) || (board[6].eql?(second_player) && board[7].eql?(second_player) && board[8].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
   elsif (board[0].eql?(board[3]) && board[3].eql?(board[6])) || (board[1].eql?(board[4]) && board[4].eql?(board[7])) || (board[2].eql?(board[5]) && board[5].eql?(board[8]))
-    if board[0].eql?(first_player) || board[1].eql?(first_player) || board[2].eql?(first_player)
+    if (board[0].eql?(first_player) && board[3].eql?(first_player) && board[6].eql?(first_player)) || (board[1].eql?(first_player) && board[4].eql?(first_player) && board[7].eql?(first_player)) || (board[2].eql?(first_player) && board[5].eql?(first_player) && board[8].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[1].eql?(second_player) || board[2].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[3].eql?(second_player) && board[6].eql?(second_player)) || (board[1].eql?(second_player) && board[4].eql?(second_player) && board[7].eql?(second_player)) || (board[2].eql?(second_player) && board[5].eql?(second_player) && board[8].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
   elsif board[0].eql?(board[4]) && board[4].eql?(board[8]) || board[2].eql?(board[4]) && board[4].eql?(board[6])
-    if board[0].eql?(first_player) || board[2].eql?(first_player)
+    if (board[0].eql?(first_player) && board[4].eql?(first_player) && board[8].eql?(first_player)) || (board[2].eql?(first_player) && board[4].eql?(first_player) && board[6].eql?(first_player))
       puts "#{player_one} is the Winner!"
       break
-    elsif board[0].eql?(second_player) || board[2].eql?(second_player)
+    elsif (board[0].eql?(second_player) && board[4].eql?(second_player) && board[8].eql?(second_player)) || (board[2].eql?(second_player) && board[4].eql?(second_player) && board[6].eql?(second_player))
       puts "#{player_two} is the Winner!"
       break
     end
