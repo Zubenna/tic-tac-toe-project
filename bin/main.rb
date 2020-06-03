3201eb09ad765ad33a8c5f841350395928625ba2
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-03 17:51:26 -0500

    Class file Delete

--- bin/main.rb
+++ bin/main.rb
@@ -31,6 +31,34 @@ loop do
   puts "  #{board[3]}  |  #{board[4]}  |  #{board[5]}"
   puts '================='
   puts "  #{board[6]}  |  #{board[7]}  |  #{board[8]}"
+  if (board[0].eql?(board[1]) && board[1].eql?(board[2])) || (board[3].eql?(board[4]) && board[4].eql?(board[5])) || (board[6].eql?(board[7]) && board[7].eql?(board[8]))
+    if board[0].eql?(first_player) || board[3].eql?(first_player) || board[6].eql?(first_player)
+      puts "#{player_one} is the Winner!"
+      break
+    elsif board[0].eql?(second_player) || board[3].eql?(second_player) || board[6].eql?(second_player)
+      puts "#{player_two} is the Winner!"
+      break
+    end
+  elsif (board[0].eql?(board[3]) && board[3].eql?(board[6])) || (board[1].eql?(board[4]) && board[4].eql?(board[7])) || (board[2].eql?(board[5]) && board[5].eql?(board[8]))
+    if board[0].eql?(first_player) || board[1].eql?(first_player) || board[2].eql?(first_player)
+      puts "#{player_one} is the Winner!"
+      break
+    elsif board[0].eql?(second_player) || board[1].eql?(second_player) || board[2].eql?(second_player)
+      puts "#{player_two} is the Winner!"
+      break
+    end
+  elsif board[0].eql?(board[4]) && board[4].eql?(board[8]) || board[2].eql?(board[4]) && board[4].eql?(board[6])
+    if board[0] == (first_player) || board[2] == (first_player)
+      puts "#{player_one} is the Winner!"
+      break
+    elsif board[0] == (second_player) || board[2] == (second_player)
+      puts "#{player_two} is the Winner!"
+      break
+    end
+  elsif board.all?(String)
+    puts 'It is a draw'
+    break
+  end
 
   loop do
     puts "#{player_one}: please select the number where you want to play from the board"
