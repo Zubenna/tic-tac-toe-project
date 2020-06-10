384a33b4518b39c2ca6ac518937f5791e9df5f9d
emiliocm9 <emilio.contreras97@gmail.com>
2020-06-10 11:02:14 -0500

    Line length corrections

--- lib/board.rb
+++ lib/board.rb
@@ -18,7 +18,6 @@ class GameBoard
     GameBoard.board[parameter] = vallid_symbol
   end
 
-  # rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/AbcSize/
   def check_winner
     # column_array_one = [0, 3, 6]
     # column_array_two = [1, 4, 7]
@@ -33,21 +32,7 @@ class GameBoard
     return true if GameBoard.board[2].eql? GameBoard.board[5] and GameBoard.board[5].eql? GameBoard.board[8]
     return true if GameBoard.board[0].eql? GameBoard.board[4] and GameBoard.board[4].eql? GameBoard.board[8]
     return true if GameBoard.board[2].eql? GameBoard.board[4] and GameBoard.board[4].eql? GameBoard.board[6]
-    # return true if (GameBoard.board[0].eql?(GameBoard.board[1]) && GameBoard.board[1].eql?(GameBoard.board[2])) || (GameBoard.board[3].eql?(GameBoard.board[4]) && GameBoard.board[4].eql?(GameBoard.board[5])) || (GameBoard.board[6].eql?(GameBoard.board[7]) && GameBoard.board[7].eql?(GameBoard.board[8]))
-    # return true if (GameBoard.board[0].eql?(GameBoard.board[3]) && GameBoard.board[3].eql?(GameBoard.board[6])) || (GameBoard.board[1].eql?(GameBoard.board[4]) && GameBoard.board[4].eql?(GameBoard.board[7])) || (GameBoard.board[2].eql?(GameBoard.board[5]) && GameBoard.board[5].eql?(GameBoard.board[8]))
-    # return true if GameBoard.board[0].eql?(GameBoard.board[4]) && GameBoard.board[4].eql?(GameBoard.board[8]) || GameBoard.board[2].eql?(GameBoard.board[4]) && GameBoard.board[4].eql?(GameBoard.board[6])
-    #test = all_equal(GameBoard.board[0..2], player_symbol) ||
-    #all_equal(GameBoard.board[3..5], player_symbol) ||
-    #all_equal(GameBoard.board[6..8], player_symbol)
-     
-    # end
-    # all_equal(column_array_one, player_symbol) ||
-    # all_equal(column_array_two, player_symbol) ||
-    # all_equal(column_array_three, player_symbol) ||
-    # all_equal(diag_array_one, player_symbol) ||
-    # all_equal(diag_array_two, player_symbol)
   end
-    # rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/AbcSize/
 
   #def all_equal(subarray, player_symbol)
   #  return true if subarray.uniq.size == 1 && subarray.uniq == player_symbol
